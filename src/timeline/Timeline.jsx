50ce4658c8a61b58ad598fe2f56b783b02127dae
Sagiteichman <chittosin@gmail.com>
2024-07-21 17:10:21 +0300

    added basics styling and icons to the side navbar

--- src/timeline/Timeline.jsx
+++ src/timeline/Timeline.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
 import './Timeline.css'
+import Sugesstions from './Sugesstions.jsx'
 
 export function Timeline() {
-  return <div>Timeline</div>
+  return (
+    <div className='timeline'>
+      timeline
+      <div className='timeline__left'></div>
+      <div className='timeline__right'>
+        <Sugesstions />
+      </div>
+    </div>
+  )
 }
