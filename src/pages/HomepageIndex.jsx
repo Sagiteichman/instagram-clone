a55f00b2ca948a0c098572b32ffb434cc89927ab
Sagiteichman <chittosin@gmail.com>
2024-07-28 15:54:45 +0300

    fixed the problem with backend comunication

--- src/pages/HomepageIndex.jsx
+++ src/pages/HomepageIndex.jsx
@@ -1,47 +1,47 @@
-import React, { useEffect, useState } from "react";
-import { Sidenav } from "../cmps/Sidenav";
-import { PostList } from "../cmps/PostList";
-import { PostCompose } from "../cmps/PostCompose.jsx";
-import { postService } from "../services/posts.service.js";
-import { userService } from "../services/users.service.js";
+import React, { useEffect, useState } from 'react'
+import { Sidenav } from '../cmps/Sidenav'
+import { PostList } from '../cmps/PostList'
+import { PostCompose } from '../cmps/PostCompose.jsx'
+import { postService } from '../services/posts.service.js'
+import { userService } from '../services/users.service.js'
 
 export function HomepageIndex() {
-  const [isComposeModalOpen, setComposeModalOpen] = useState(false);
-  const [posts, setPosts] = useState([]);
-  const [user, setUser] = useState(null);
-  const [editedPostId, setEditedPostId] = useState(null);
+  const [isComposeModalOpen, setComposeModalOpen] = useState(false)
+  const [posts, setPosts] = useState([])
+  const [user, setUser] = useState(null)
+  const [editedPostId, setEditedPostId] = useState(null)
 
-  const currentUserPosts = posts.filter((post) => post.user.id === user.id);
+  const currentUserPosts = posts.filter((post) => post.user.id === user.id)
 
   const fetchPosts = async () => {
-    const posts = await postService.getPosts();
-    setPosts(posts);
-  };
+    const posts = await postService.getPosts()
+    setPosts(posts)
+  }
 
   const fetchUser = async () => {
-    const user = await userService.getUserById("1");
-    setUser(user);
-  };
+    const user = await userService.getUserById('1')
+    setUser(user)
+  }
 
   useEffect(() => {
-    fetchPosts();
-    fetchUser();
-  }, []);
+    fetchPosts()
+    fetchUser()
+  }, [])
 
-  const editedPost = posts.find((post) => post.id === editedPostId);
-  const shouldShowComposeModal = editedPost || isComposeModalOpen;
+  const editedPost = posts.find((post) => post.id === editedPostId)
+  const shouldShowComposeModal = editedPost || isComposeModalOpen
 
   const toggleModal = () => {
-    setComposeModalOpen(!shouldShowComposeModal);
-    setEditedPostId(null);
-  };
+    setComposeModalOpen(!shouldShowComposeModal)
+    setEditedPostId(null)
+  }
 
   return (
-    <div className="homepage">
-      <div className="homepage__nav">
+    <div className='homepage'>
+      <div className='homepage__nav'>
         <Sidenav toggleModal={toggleModal} />
       </div>
-      <div className="homepage__timeline">
+      <div className='homepage__timeline'>
         <PostList
           posts={posts}
           fetchPosts={fetchPosts}
@@ -57,5 +57,5 @@ export function HomepageIndex() {
         post={editedPost}
       />
     </div>
-  );
+  )
 }
