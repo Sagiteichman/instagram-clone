26c096e8eee5b07089f337fe63b5cc510f8d5d91
Sagiteichman <chittosin@gmail.com>
2024-07-29 21:17:45 +0300

    added demi comments

--- src/cmps/PostDetails.jsx
+++ src/cmps/PostDetails.jsx
@@ -1,38 +1,48 @@
-import { useSearchParams } from "react-router-dom";
-import { Author } from "./Author";
+import { useSearchParams } from 'react-router-dom'
+import { Author } from './Author'
 
 export const PostDetails = ({ selectedPost }) => {
-  const [params, setParams] = useSearchParams();
+  const [params, setParams] = useSearchParams()
 
   const closeModal = () => {
-    params.delete("postId");
-    setParams(params);
-  };
+    params.delete('postId')
+    setParams(params)
+  }
 
-  const exampleComments = new Array(10).fill(undefined);
+  const exampleComments = new Array(7).fill(undefined)
   return (
     <>
       {selectedPost && (
-        <div className="modal">
-          <div onClick={closeModal} className="overlay"></div>
-          <div className="postDetails modal__content">
-            <div className="img">
-              <img src={selectedPost.postImage} alt="Selected" />
+        <div className='modal'>
+          <div onClick={closeModal} className='overlay'></div>
+          <div className='postDetails modal__content'>
+            <div className='img'>
+              <img src={selectedPost.postImage} alt='Selected' />
             </div>
-            <div className="postDetails__comments">
+            <div className='postDetails__comments'>
               <Author
                 name={selectedPost.user.name}
                 imageUrl={selectedPost.user?.imageUrl}
                 timestamp={selectedPost.timestamp}
               />
-              {exampleComments.map((_, i) => (
-                <div key={i} className="comment">
-                  <Author
-                    name={selectedPost.user.name}
-                    imageUrl={selectedPost.user?.imageUrl}
-                    timestamp={selectedPost.timestamp}
-                  />
-                  <p>{selectedPost.text}</p>
+
+              {selectedPost.comments.map((_, i) => (
+                <div key={i} className='comment'>
+                  <div className='comment_name_and_text'>
+                    <Author
+                      name={_.userName}
+                      imageUrl={_.userImgUrl}
+                      timestamp={_.timestamp}
+                    />
+                    <span>{_.text}</span>
+                  </div>
+                  <div>
+                    <span>{_.timestamp}</span>
+                    <span>{_.likes > 0 && _.likes + 'likes'}</span>
+                    <span>Reply</span>
+                    <span>See translation</span>
+                    <span>...</span>
+                  </div>
                 </div>
               ))}
             </div>
@@ -40,5 +50,5 @@ export const PostDetails = ({ selectedPost }) => {
         </div>
       )}
     </>
-  );
-};
+  )
+}
