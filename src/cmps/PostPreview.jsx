1dd003adaaaa4f9036788ca68a7bdaa9f3563a4c
Sagiteichman <chittosin@gmail.com>
2024-07-31 02:57:46 +0300

    added some styling but add post not working

--- src/cmps/PostPreview.jsx
+++ src/cmps/PostPreview.jsx
@@ -1,17 +1,15 @@
-import React, { useState } from "react";
-import { Avatar } from "@mui/material";
-import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
-import LikeIcon from "../assets/svg/Like.jsx";
-import { LikeFilled } from "../assets/svg/LikeFilled.jsx";
-import CommentIcon from "../assets/svg/Comment.jsx";
-import ShareIcon from "../assets/svg/Share.jsx";
-import SaveIcon from "../assets/svg/Save.jsx";
-import { PostMenu } from "./PostOptions";
-import { postService } from "../services/posts.service";
-import { timeSince } from "../services/timeSince";
-import { useSearchParams } from "react-router-dom";
-import { Author } from "./Author.jsx";
-import { AddComment } from "./AddComment.jsx";
+import React, { useState } from 'react'
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz'
+import LikeIcon from '../assets/svg/Like.jsx'
+import { LikeFilled } from '../assets/svg/LikeFilled.jsx'
+import CommentIcon from '../assets/svg/Comment.jsx'
+import ShareIcon from '../assets/svg/Share.jsx'
+import SaveIcon from '../assets/svg/Save.jsx'
+import { PostMenu } from './PostOptions'
+import { postService } from '../services/posts.service'
+import { useSearchParams } from 'react-router-dom'
+import { Author } from './Author.jsx'
+import { AddComment } from './AddComment.jsx'
 
 function PostPreview({
   id,
@@ -21,49 +19,49 @@ function PostPreview({
   timestamp,
   fetchPosts,
   comments,
-  text = "",
+  text = '',
   setEditedPostId,
   currentUser,
 }) {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [params, setParams] = useSearchParams();
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const [params, setParams] = useSearchParams()
 
   const openDetails = () => {
-    params.append("postId", id);
-    setParams(params);
-  };
+    params.append('postId', id)
+    setParams(params)
+  }
 
   const handleDelete = async () => {
-    console.log("delete post", id);
-    const result = await postService.deletePost(id);
+    console.log('delete post', id)
+    const result = await postService.deletePost(id)
     if (result.success) {
-      fetchPosts();
-      console.log("post deleted", result);
-      setIsMenuOpen(false);
+      fetchPosts()
+      console.log('post deleted', result)
+      setIsMenuOpen(false)
     } else {
-      console.log("error deleting post", result);
+      console.log('error deleting post', result)
     }
-  };
+  }
 
   const handleEditClick = () => {
-    setEditedPostId(id);
-    setIsMenuOpen(false);
-  };
+    setEditedPostId(id)
+    setIsMenuOpen(false)
+  }
 
-  const isLiked = likes.includes(currentUser?.id);
+  const isLiked = likes.includes(currentUser?.id)
 
   const onLikeClick = async () => {
     await postService.editPost(id, {
       likes: isLiked
         ? likes.filter((like) => like !== currentUser.id)
         : [...likes, currentUser.id],
-    });
-    await fetchPosts();
-  };
+    })
+    await fetchPosts()
+  }
 
   return (
-    <div className="post">
-      <div className="post__header">
+    <div className='post'>
+      <div className='post__header'>
         <Author
           name={user?.name}
           imageUrl={user?.imageUrl || postImage}
@@ -79,42 +77,45 @@ function PostPreview({
           />
         )}
       </div>
-      <div className="post__image">
-        <img onClick={openDetails} src={postImage} alt="Upload" />
+      <div className='post__image'>
+        <img onClick={openDetails} src={postImage} alt='Upload' />
       </div>
-      <div className="post__footer">
-        <div className="post__footerIcons">
-          <div className="post__iconsMain">
+      <div className='post__footer'>
+        <div className='post__footerIcons'>
+          <div className='post__iconsMain'>
             {isLiked ? (
-              <LikeFilled className="postIcon" onClick={onLikeClick} />
+              <LikeFilled className='postIcon' onClick={onLikeClick} />
             ) : (
-              <LikeIcon className="postIcon" onClick={onLikeClick} />
+              <LikeIcon className='postIcon' onClick={onLikeClick} />
             )}
-            <CommentIcon className="postIcon" />
-            <ShareIcon className="postIcon" />
+            <CommentIcon className='postIcon' />
+            <ShareIcon className='postIcon' />
           </div>
-          <div className="post__iconsSave">
-            <SaveIcon className="postIcon" />
+          <div className='post__iconsSave'>
+            <SaveIcon className='postIcon' />
           </div>
         </div>
-        {likes.length > 0 &&
-          `${likes.length} like${likes.length > 1 ? "s" : ""}`}
-        <br />
-        {user?.name || "Guest"} <br />
-        <br />
-        <div>
-          {comments?.slice(0, 3)?.map((comment) => {
-            return (
-              <div>
-                {comment.user.name}: {comment.text}
-              </div>
-            );
-          })}
+        <div className='foother__description'>
+          <span className='footer__likes'>
+            {likes.length > 0 &&
+              `${likes.length} like${likes.length > 1 ? 's' : ''}`}
+          </span>
+          <span className='footer__username'>{user?.name || 'Guest'}</span>
         </div>
+        {comments?.slice(0, 3)?.map((comment) => {
+          return (
+            <div className='footer__comments'>
+              {comment.user.name}:
+              <span className='post__comment'>{comment.text}</span>
+            </div>
+          )
+        })}
         {comments?.length && (
           <div onClick={openDetails}>View all {comments?.length} comments</div>
         )}
-        <span className="post__description">{text}</span>
+        <span className='post__description'>{text}</span>
+      </div>
+      <div>
         <AddComment
           postId={id}
           commenterId={currentUser.id}
@@ -123,7 +124,7 @@ function PostPreview({
         />
       </div>
     </div>
-  );
+  )
 }
 
-export default PostPreview;
+export default PostPreview
--- src/cmps/Sidenav.jsx
+++ src/cmps/Sidenav.jsx
@@ -1,130 +1,127 @@
-import React from "react";
-import { useNavigate, useLocation } from "react-router-dom";
+import React from 'react'
+import { useNavigate, useLocation } from 'react-router-dom'
 
 // Icons
-import HomeIcon from "../assets/svg/HomeFocus.jsx";
-import SearchIcon from "../assets/svg/Search.jsx";
-import ExploreIcon from "../assets/svg/Explore.jsx";
-import ReelsIcon from "../assets/svg/Reels.jsx";
-import ChatIcon from "../assets/svg/Messenger.jsx";
-import LikeIcon from "../assets/svg/Like.jsx";
-import CreateIcon from "../assets/svg/Create.jsx";
-import MoreIcon from "../assets/svg/Hamburger.jsx";
-import { Avatar } from "@mui/material";
+import HomeIcon from '../assets/svg/HomeFocus.jsx'
+import SearchIcon from '../assets/svg/Search.jsx'
+import ExploreIcon from '../assets/svg/Explore.jsx'
+import ReelsIcon from '../assets/svg/Reels.jsx'
+import ChatIcon from '../assets/svg/Messenger.jsx'
+import LikeIcon from '../assets/svg/Like.jsx'
+import CreateIcon from '../assets/svg/Create.jsx'
+import MoreIcon from '../assets/svg/Hamburger.jsx'
+import { Avatar } from '@mui/material'
 
 function Sidenav({ toggleModal, user }) {
-  const navigate = useNavigate();
-  const { pathname } = useLocation();
-  console.log("pathname", pathname);
+  const navigate = useNavigate()
+  const { pathname } = useLocation()
+  console.log('pathname', pathname)
 
   const handleNavigate = (path) => {
-    navigate(path);
-  };
+    navigate(path)
+  }
 
   return (
-    <div className="sidenav">
-      {/* <img
-        className='sidenav__logo'
-        src='https://www.pngkey.com/png/full/828-8286178_mackeys-work-needs-no-elaborate-presentation-or-distraction.png'
-        alt='insagram logo'
-      /> */}
-      <h1 className="sidenav__logo">Bellingram</h1>
-      <div className="sidenav__buttons">
+    <div className='sidenav'>
+      <h2 className='sidenav__logo'>Bellingram</h2>
+      <div className='sidenav__buttons'>
         <button
-          className={"sidenav__button" + (pathname === "/" ? " selected" : "")}
-          onClick={() => handleNavigate("/")}
+          className={'sidenav__button' + (pathname === '/' ? ' selected' : '')}
+          onClick={() => handleNavigate('/')}
         >
-          <HomeIcon fill="white" />
-          <span>Home</span>
+          <HomeIcon fill='white' />
+          <span className='sidenav__text'>Home</span>
         </button>
 
         <button
           className={
-            "sidenav__button" + (pathname === "/search" ? " selected" : "")
+            'sidenav__button' + (pathname === '/search' ? ' selected' : '')
           }
         >
           <SearchIcon />
-          <span>Search</span>
+          <span className='sidenav__text'>Search</span>
         </button>
 
         <button
           className={
-            "sidenav__button" + (pathname === "/explore" ? " selected" : "")
+            'sidenav__button' + (pathname === '/explore' ? ' selected' : '')
           }
         >
           <ExploreIcon />
-          <span>Explore</span>
+          <span className='sidenav__text'>Explore</span>
         </button>
 
         <button
           className={
-            "sidenav__button" + (pathname === "/reels" ? " selected" : "")
+            'sidenav__button' + (pathname === '/reels' ? ' selected' : '')
           }
         >
           <ReelsIcon />
-          <span>Reels</span>
+          <span className='sidenav__text'>Reels</span>
         </button>
 
         <button
           className={
-            "sidenav__button" + (pathname === "/messages" ? " selected" : "")
+            'sidenav__button' + (pathname === '/messages' ? ' selected' : '')
           }
         >
           <ChatIcon />
-          <span>Messages</span>
+          <span className='sidenav__text'>Messages</span>
         </button>
 
         <button
           className={
-            "sidenav__button" +
-            (pathname === "/notifications" ? " selected" : "")
+            'sidenav__button' +
+            (pathname === '/notifications' ? ' selected' : '')
           }
         >
           <LikeIcon />
-          <span>Notifications</span>
+          <span className='sidenav__text'>Notifications</span>
         </button>
 
         <button
           className={
-            "sidenav__button" + (pathname === "/create" ? " selected" : "")
+            'sidenav__button' + (pathname === '/create' ? ' selected' : '')
           }
           onClick={toggleModal}
         >
           <CreateIcon />
-          <span>Create</span>
+          <span className='sidenav__text'>Create</span>
         </button>
 
         <button
           className={
-            "sidenav__button avatar__button" +
-            (pathname === "/profile" ? " selected" : "")
+            'sidenav__button avatar__button' +
+            (pathname === '/profile' ? ' selected' : '')
           }
-          onClick={() => handleNavigate("/profile")}
+          onClick={() => handleNavigate('/profile')}
         >
           {user && (
             <Avatar
-              className="sidenav__avatar"
+              className='sidenav__avatar'
               src={user.imageUrl}
-              alt={user.name || "Guest"}
+              alt={user.name || 'Guest'}
             >
               {/* {!postImage && user.charAt(0).toUpperCase()} */}
             </Avatar>
           )}
-          <span>Profile</span>
+          <span className='sidenav__text'>Profile</span>
         </button>
       </div>
 
-      <div className="sidenav_spacer"></div>
-      <div className="sidenav__more">
+      <div className='sidenav_spacer'></div>
+      <div className='sidenav__more'>
         <button
-          className={"sidenav__button" + (pathname === "/" ? " selected" : "")}
+          className={
+            'sidenav__button' + (pathname === '/more' ? ' selected' : '')
+          }
         >
           <MoreIcon />
-          <span>More</span>
+          <span className='sidenav__text'>More</span>
         </button>
       </div>
     </div>
-  );
+  )
 }
 
-export default Sidenav;
+export default Sidenav
