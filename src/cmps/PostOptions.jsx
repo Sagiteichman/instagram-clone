eac12d94f2201c3ffe5233866e42ba56ff4c3e22
Sagiteichman <chittosin@gmail.com>
2024-07-30 16:22:39 +0300

    added style to postOptions

--- src/cmps/PostOptions.jsx
+++ src/cmps/PostOptions.jsx
@@ -6,17 +6,21 @@ export function PostMenu({ isOpen, setIsOpen, onDeleteClick, onEditClick }) {
   const handleClose = () => setIsOpen(false)
 
   return (
-    <Modal open={isOpen} onClose={handleClose}>
+    <div className='modal'>
+      <div onClick={handleClose} open={isOpen} className='overlay'></div>
       <div className='post__options'>
         <div className='post__buttons'>
-          <button className='post__options__button' onClick={onDeleteClick}>
-            delete
+          <button
+            className='post__options__button options_delete_button'
+            onClick={onDeleteClick}
+          >
+            <span>Delete</span>
           </button>
           <button className='post__options__button' onClick={onEditClick}>
             Edit
           </button>
         </div>
       </div>
-    </Modal>
+    </div>
   )
 }
